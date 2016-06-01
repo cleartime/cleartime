1e13db8b3f84989aa503fd6459151d9f63caf858
gxx <562606139@qq.com>
2016-06-01 18:24:44 +0800

    6.1

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
+//设置跨域处理头部
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3002");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
