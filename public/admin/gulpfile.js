264aefba63c5a743ea942e14043ca926675c10f1
gxx <562606139@qq.com>
2016-06-13 14:15:15 +0800

    已完善文章(上传图片未完成)

--- public/admin/gulpfile.js
+++ public/admin/gulpfile.js
@@ -25,13 +25,13 @@ var cssSprite = require('gulp-css-spritesmith');
 gulp.task('autoSprite', function() {
     gulp.src('css/*.css').pipe(cssSprite({
             // sprite背景图源文件夹，只有匹配此路径才会处理，默认 images/slice/
-            imagepath: 'src/img',
+            imagepath: './src/img',
             // 映射CSS中背景路径，支持函数和数组，默认为 null
             imagepath_map: null,
             // 雪碧图输出目录，注意，会覆盖之前文件！默认 images/
-            spritedest: 'www/img',
+            spritedest: './www/img',
             // 替换后的背景路径，默认 ../images/
-            spritepath: '../images/',
+            spritepath: './www/img',
             // 各图片间间距，如果设置为奇数，会强制+1以保证生成的2x图片为偶数宽高，默认 0
             padding: 2,
             // 是否使用 image-set 作为2x图片实现，默认不使用
@@ -43,7 +43,7 @@ gulp.task('autoSprite', function() {
             // 在CSS文件末尾追加时间戳，默认不追加
             cssstamp: true
         }))
-        .pipe(gulp.dest('publish/'));
+        .pipe(gulp.dest('./www/img'));
 });
 
 
@@ -110,7 +110,7 @@ gulp.task('watch',['browsersync'], function () {
 });
 
 //default task
-gulp.task('default', ['watch','compass','script','html','lib'], function () {
+gulp.task('default', ['watch','compass','script','html','lib','autoSprite'], function () {
     //browserSync.init({
     //    server: {
     //        baseDir: "./"
