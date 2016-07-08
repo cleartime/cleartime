ed5bb12791129258abe502f0cac86057a6f2c642
gxx <562606139@qq.com>
2016-07-08 10:30:43 +0800

    加了点有越的东西

--- public/blog/src/App.vue
+++ public/blog/src/App.vue
@@ -18,6 +18,18 @@ import welcome from './views/welcome';
 import store from './vuex/store';
 
 export default {
+  ready() {
+    // 会变的 title
+    document.addEventListener('visibilitychange', () => {
+      document.title = document.hidden ? '出BUG了，快看！' : '我的博客';
+    });
+    // 控制台
+    try {
+      console.log('一个人到底多无聊\r\n 才会把 console 当成玩具\r\n一个人究竟多堕落\r\n 才会把大好青春荒废在博客上\r\n\r\n\r\n%cfollow me %c https://github.com/cleartime', 'color:red', 'color:green');
+    } catch (e) {
+      console.log(e);
+    }
+  },
   data() {
     return {
       isShow: true,
