dc35c798b301131ebbe11398fed63e0b254f953c
cleartime <562606139@qq.com>
2016-07-21 14:50:08 +0800

    7.21

--- public/blog/src/config.js
+++ public/blog/src/config.js
@@ -1,5 +1,5 @@
 export default (() => {
-  const url = 'http://localhost:3000';
-  // const url = 'http://cleartime.leanapp.cn';
+  // const url = 'http://localhost:3000';
+  const url = 'http://cleartime.leanapp.cn';
   return url;
 })();
