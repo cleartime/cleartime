5221fc19cdd2faf1db0d6e99de6b69080e525aa7
cleartime <562606139@qq.com>
2016-11-08 15:44:21 +0800

    改为https

--- public/blog/src/config.js
+++ public/blog/src/config.js
@@ -1,7 +1,7 @@
 export default (() => {
   /* eslint-disable indent, spaced-comment */
    //const url = 'http://localhost:3000';
-  const url = 'http://gxx.leanapp.cn';
+  const url = 'https://gxx.leanapp.cn';
   /* eslint-enable indent, spaced-comment */
   return url;
 })();
