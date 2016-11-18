fd833ebe0795b568562e618a9cc909b92a738f39
cleartime <562606139@qq.com>
2016-11-18 10:54:26 +0800

    用vue2.0重构中

--- public/admin/src/js/config.js
+++ public/admin/src/js/config.js
@@ -2,8 +2,8 @@
  * Created by Laggo on 11/4/15.
  */
 var config = {
-    //'SERVER_URL': 'http://localhost:3000'
-    'SERVER_URL': 'https://gxx.leanapp.cn'
+    'SERVER_URL': 'http://localhost:3000'
+    //'SERVER_URL': 'https://gxx.leanapp.cn'
 };
 for(item in config){
     app.constant(item,config[item])
