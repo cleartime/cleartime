c3bb2f8637745ef299233d8b6d072092bcc5cc07
cleartime <562606139@qq.com>
2016-11-24 09:50:34 +0800

    用vue2.0重构中

--- public/blog/src/main.js
+++ public/blog/src/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue';
 import App from './App';
 import VueRouter from 'vue-router';
-// import configRouter from './routers';
+import configRouter from './routers';
 import { timeToNow, timeToUpdata } from './filters';
-// import { fetchMsgCount } from './vuex/actions';
-// import { getToken } from './vuex/getters';
 import store from './vuex/store';
 
 Vue.filter('timeToNow', timeToNow);
@@ -12,39 +10,7 @@ Vue.filter('timeToUpdata', timeToUpdata);
 
 Vue.use(VueRouter);
 
-const router = new VueRouter({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      component: require('./views/page'),
-      meta: {
-        isindex: true,
-      },
-    },
-    {
-      path: '/welcome',
-      component: require('./views/welcome'),
-      meta: {
-        isWelcome: true,
-      },
-    },
-    {
-      path: '/post',
-      component: require('./views/post'),
-      meta: {
-        isPost: true,
-      },
-    },
-    {
-      path: '/me',
-      component: require('./views/me'),
-      meta: {
-        isMe: true,
-      },
-    },
-  ],
-});
+const router = new VueRouter(configRouter);
 
 /* eslint-disable no-new */
 new Vue({
--- public/blog/src/routers.js
+++ public/blog/src/routers.js
@@ -1,2 +1,33 @@
-
-export default (router) => router;
+export default {
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: require('./views/page'),
+      meta: {
+        isindex: true,
+      },
+    },
+    {
+      path: '/welcome',
+      component: require('./views/welcome'),
+      meta: {
+        isWelcome: true,
+      },
+    },
+    {
+      path: '/post',
+      component: require('./views/post'),
+      meta: {
+        isPost: true,
+      },
+    },
+    {
+      path: '/me',
+      component: require('./views/me'),
+      meta: {
+        isMe: true,
+      },
+    },
+  ],
+};
