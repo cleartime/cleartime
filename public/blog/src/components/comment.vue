8144cbd169d737eaacee5284bdb4979a6c3f3f26
cleartime <562606139@qq.com>
2017-01-16 13:47:51 +0800

    重构完成

--- public/blog/src/components/comment.vue
+++ public/blog/src/components/comment.vue
@@ -7,6 +7,7 @@
         <div class="comment"  v-if = '!i.fid'>
           <div class="comment-pep">
             <i class="iconfont icon-myline"></i>
+
             <!--<img src="../assets/pep.png">-->
             <p>{{ i.nickname }}</p>
           </div>
